af0d8e961cb13f724c1a770e8823525d0d00d95f
Sanjiino <romain.cosson11@live.fr>
2017-03-09 16:44:47 +0100

    rappels terminés

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,26 @@
 //REVISIONS
 // Declarez une variable nommée "boucler" contenant true
-
+var boucler = true;
 
 // Declarez un tableau members contenant Aida67, lapie002, anneserrano, Jennysmille, nunkabuk, RCosson, kaonb-ax, FerEmilie, crazychouwi, KiluaZoldyc, patatobeur, Sam11360, elo062, hermeline, Biciclet,
-
+var members = ['Aida67', 'lapie002', 'anneserrano', 'Jennysmille', 'nunkabuk', 'RCosson', 'kaonb-ax', 'FerEmilie', 'crazychouwi', 'KiluaZoldyc', 'patatobeur', 'Sam11360', 'elo062', 'hermeline', 'Biciclet'];
 // SI la variable boucler vaut true ALORS
-
+$("#check").click(function(){
+  if (boucler === true) {
+    for (var i = 0; i < members.length; i++) {
+      switch (members[i]) {
+        case "RCosson":
+            console.log("affiche " + members[i]);
+            $( ".test" ).append("<p>" + "affiche " + members[i] + "</p>");
+          break;
+        default:
+            console.log(members[i]);
+            $( ".test" ).append("<p>" + members[i] + "</p>");
+          break;
+      }
+    }
+  }
+})
   // Bouclez sur le tableau que vous avez déclaré ci-dessus
 
   // Mettre un switch pour qu'au moment où la boucle se trouve sur votre pseudo cela ajoute "Affiche " devant votre pseudo dans la console et sur l'écran et par defaut seulement le pseudo des autres
