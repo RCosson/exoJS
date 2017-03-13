e1cc87008c3873313b67d7a752a7b97680c0e638
Sanjiino <romain.cosson11@live.fr>
2017-03-13 14:18:29 +0100

    exercice ajax terminé

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,15 @@
 // REVISIONS
 // Declarez une variable nommée "boucler" contenant true
-<<<<<<< HEAD
 var boucler = true;
 
 // Declarez un tableau members contenant Aida67, lapie002, anneserrano, Jennysmille, nunkabuk, RCosson, kaonb-ax, FerEmilie, crazychouwi, KiluaZoldyc, patatobeur, Sam11360, elo062, hermeline, Biciclet,
 var members = ['Aida67', 'lapie002', 'anneserrano', 'Jennysmille', 'nunkabuk', 'RCosson', 'kaonb-ax', 'FerEmilie', 'crazychouwi', 'KiluaZoldyc', 'patatobeur', 'Sam11360', 'elo062', 'hermeline', 'Biciclet'];
-=======
 //
 //
 // Declarez un tableau members contenant Aida67, lapie002, anneserrano, Jennysmille, nunkabuk, RCosson, kaonb-ax, FerEmilie, crazychouwi, KiluaZoldyc, patatobeur, Sam11360, elo062, hermeline, Biciclet,
 //
->>>>>>> 26697ae42279b3c40455189642c0752911625fed
 // SI la variable boucler vaut true ALORS
-$("#check").click(function(){
+/*$("#check").click(function(){
   if (boucler === true) {
     for (var i = 0; i < members.length; i++) {
       switch (members[i]) {
@@ -27,7 +24,7 @@ $("#check").click(function(){
       }
     }
   }
-})
+})*/
   // Bouclez sur le tableau que vous avez déclaré ci-dessus
   // Mettre un switch pour qu'au moment où la boucle se trouve sur votre pseudo cela ajoute "Affiche " devant votre pseudo dans la console et sur l'écran et par defaut seulement le pseudo pour les autres
 
@@ -35,6 +32,15 @@ $("#check").click(function(){
 //
 // COURS AJAX
 // Faire une requete en ajax pour charger de façon asynchrone le html du fichier contenu.html
+
+$(function() {
+  $('#bouton').click(function() {
+    $('#resultatAjax').load('contenu.html', function() {
+      console.log("Chargement réussi");
+    });
+  });
+});
+
 
 // COURS AJAX vers des API
   // AJAX Jquery .ajax() ou .getJSON()
